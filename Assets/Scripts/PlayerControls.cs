ddbca4a9adf74f61252ad86cf8795ec9b90d6a8d
longshiplegion <amyers0107@gmail.com>
2023-03-13 13:08:03 -0500

    Added more to project after hiatus

---
+++ Assets/Scripts/PlayerControls.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControls : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+      float xThrow =  Input.GetAxis("Horizontal");
+        Debug.Log(xThrow);
+        float yThrow = Input.GetAxis("Vertical");
+        Debug.Log(yThrow);
+
+    }
+}
