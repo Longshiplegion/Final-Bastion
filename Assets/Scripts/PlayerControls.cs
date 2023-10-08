c530bc8b37d32e07ad5cf4d2234f77d2c03d5520
longshiplegion <amyers0107@gmail.com>
2023-10-07 21:16:21 -0500

    final changes switching to desktop for future useage

--- Assets/Scripts/PlayerControls.cs
+++ Assets/Scripts/PlayerControls.cs
@@ -4,19 +4,50 @@ using UnityEngine;
 
 public class PlayerControls : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
+    [SerializeField] float controlSpeed = 1f;
+    [SerializeField] float xRange = 5f;
+    [SerializeField] float yRange = 5f;
+    [SerializeField] float postitionPitchFactor = -2f;
+    [SerializeField] float controlPitchFactor = -10f;
+    [SerializeField] float PositionYawFactor = -2f;
+    [SerializeField] float controlRollFactor = -5f;
+    float xThrow;
+    float yThrow;
     // Update is called once per frame
     void Update()
     {
-      float xThrow =  Input.GetAxis("Horizontal");
-        Debug.Log(xThrow);
-        float yThrow = Input.GetAxis("Vertical");
-        Debug.Log(yThrow);
+        ProcessTranslation();
+        ProcessRotation();
 
+    }
+
+    void ProcessRotation()
+    {
+        float pitchDueToPosition = transform.localPosition.y * postitionPitchFactor;
+        float pitchDueToControl = yThrow * controlPitchFactor;
+        float pitch = pitchDueToPosition + pitchDueToControl;
+        float yaw = transform.localPosition.x * PositionYawFactor;
+        float roll = xThrow * controlRollFactor;
+        transform.localRotation = Quaternion.Euler(-pitch,yaw,roll);
+    }
+    void ProcessTranslation()
+    {
+         xThrow = Input.GetAxis("Horizontal");
+
+         yThrow = Input.GetAxis("Vertical");
+
+        float xOffset = xThrow * Time.deltaTime * controlSpeed;
+
+        float yOffset = yThrow * Time.deltaTime * controlSpeed;
+
+
+        float rawXPos = transform.localPosition.x + xOffset;
+
+        float rawYPos = transform.localPosition.y + yOffset;
+
+        float clampedXpos = Mathf.Clamp(rawXPos, -xRange, xRange);
+        float clampedYpos = Mathf.Clamp(rawYPos, -yRange, yRange);
+
+        transform.localPosition = new Vector3(clampedXpos, clampedYpos, transform.localPosition.z);
     }
 }
